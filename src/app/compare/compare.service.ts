57e3e727f560b0013c234b768d591068078c76e7
vkyscen <vickyeditz@gmail.com>
2020-05-14 16:25:35 +0530

    Adding user data post request completed

--- src/app/compare/compare.service.ts
+++ src/app/compare/compare.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/map';
+import { Injectable } from "@angular/core";
+import { Http, Headers, Response } from "@angular/http";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Subject } from "rxjs/Subject";
+import "rxjs/add/operator/map";
 
-import { CompareData } from './compare-data.model';
-import { AuthService } from '../user/auth.service';
+import { CompareData } from "./compare-data.model";
+import { AuthService } from "../user/auth.service";
 
 @Injectable()
 export class CompareService {
@@ -14,18 +14,27 @@ export class CompareService {
   dataLoaded = new Subject<CompareData[]>();
   dataLoadFailed = new Subject<boolean>();
   userData: CompareData;
-  constructor(private http: Http,
-              private authService: AuthService) {
-  }
+  constructor(private http: Http, private authService: AuthService) {}
 
   onStoreData(data: CompareData) {
     this.dataLoadFailed.next(false);
     this.dataIsLoading.next(true);
     this.dataEdited.next(false);
     this.userData = data;
-      this.http.post('https://API_ID.execute-api.REGION.amazonaws.com/dev/', data, {
-        headers: new Headers({'Authorization': 'XX'})
-      })
+    this.authService.getAuthenticatedUser().getSession((err, session) => {
+      if (err) {
+        return;
+      }
+      this.http
+        .post(
+          "https://96mex4cj0j.execute-api.ap-south-1.amazonaws.com/comp-dev/comparison",
+          data,
+          {
+            headers: new Headers({
+              Authorization: session.getIdToken().getJwtToken(),
+            }),
+          }
+        )
         .subscribe(
           (result) => {
             this.dataLoadFailed.next(false);
@@ -38,51 +47,57 @@ export class CompareService {
             this.dataEdited.next(false);
           }
         );
+    });
   }
   onRetrieveData(all = true) {
     this.dataLoaded.next(null);
     this.dataLoadFailed.next(false);
-      let queryParam = '';
-      let urlParam = 'all';
-      if (!all) {
-        urlParam = 'single';
-      }
-      this.http.get('https://API_ID.execute-api.REGION.amazonaws.com/dev/' + urlParam + queryParam, {
-        headers: new Headers({'Authorization': 'XXX'})
-      })
-        .map(
-          (response: Response) => response.json()
-        )
-        .subscribe(
-          (data) => {
-            if (all) {
-              this.dataLoaded.next(data);
-            } else {
-              console.log(data);
-              if (!data) {
-                this.dataLoadFailed.next(true);
-                return;
-              }
-              this.userData = data[0];
-              this.dataEdited.next(true);
+    let queryParam = "";
+    let urlParam = "all";
+    if (!all) {
+      urlParam = "single";
+    }
+    this.http
+      .get(
+        "https://API_ID.execute-api.REGION.amazonaws.com/dev/" +
+          urlParam +
+          queryParam,
+        {
+          headers: new Headers({ Authorization: "XXX" }),
+        }
+      )
+      .map((response: Response) => response.json())
+      .subscribe(
+        (data) => {
+          if (all) {
+            this.dataLoaded.next(data);
+          } else {
+            console.log(data);
+            if (!data) {
+              this.dataLoadFailed.next(true);
+              return;
             }
-          },
-          (error) => {
-            this.dataLoadFailed.next(true);
-            this.dataLoaded.next(null);
+            this.userData = data[0];
+            this.dataEdited.next(true);
           }
-        );
+        },
+        (error) => {
+          this.dataLoadFailed.next(true);
+          this.dataLoaded.next(null);
+        }
+      );
   }
   onDeleteData() {
     this.dataLoadFailed.next(false);
-      this.http.delete('https://API_ID.execute-api.REGION.amazonaws.com/dev/', {
-        headers: new Headers({'Authorization': 'XXX'})
+    this.http
+      .delete("https://API_ID.execute-api.REGION.amazonaws.com/dev/", {
+        headers: new Headers({ Authorization: "XXX" }),
       })
-        .subscribe(
-          (data) => {
-            console.log(data);
-          },
-          (error) => this.dataLoadFailed.next(true)
-        );
+      .subscribe(
+        (data) => {
+          console.log(data);
+        },
+        (error) => this.dataLoadFailed.next(true)
+      );
   }
 }
