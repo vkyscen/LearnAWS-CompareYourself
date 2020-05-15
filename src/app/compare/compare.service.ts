c6783837bcb4527650690e4ed21951dc5fa09419
vkyscen <vickyeditz@gmail.com>
2020-05-15 18:41:49 +0530

    Retrive data completed

--- src/app/compare/compare.service.ts
+++ src/app/compare/compare.service.ts
@@ -52,47 +52,59 @@ export class CompareService {
   onRetrieveData(all = true) {
     this.dataLoaded.next(null);
     this.dataLoadFailed.next(false);
-    let queryParam = "";
-    let urlParam = "all";
-    if (!all) {
-      urlParam = "single";
-    }
-    this.http
-      .get(
-        "https://API_ID.execute-api.REGION.amazonaws.com/dev/" +
-          urlParam +
-          queryParam,
-        {
-          headers: new Headers({ Authorization: "XXX" }),
-        }
-      )
-      .map((response: Response) => response.json())
-      .subscribe(
-        (data) => {
-          if (all) {
-            this.dataLoaded.next(data);
-          } else {
-            console.log(data);
-            if (!data) {
-              this.dataLoadFailed.next(true);
-              return;
-            }
-            this.userData = data[0];
-            this.dataEdited.next(true);
+    this.authService.getAuthenticatedUser().getSession((err, session) => {
+      const queryParam =
+        "?accessToken=" + session.getAccessToken().getJwtToken();
+      let urlParam = "all";
+      if (!all) {
+        urlParam = "single";
+      }
+      this.http
+        .get(
+          "https://96mex4cj0j.execute-api.ap-south-1.amazonaws.com/comp-dev/comparison/" +
+            urlParam +
+            queryParam,
+          {
+            headers: new Headers({
+              Authorization: session.getIdToken().getJwtToken(),
+            }),
           }
-        },
-        (error) => {
-          this.dataLoadFailed.next(true);
-          this.dataLoaded.next(null);
-        }
-      );
+        )
+        .map((response: Response) => response.json())
+        .subscribe(
+          (data) => {
+            if (all) {
+              this.dataLoaded.next(data);
+            } else {
+              console.log(data);
+              if (!data) {
+                this.dataLoadFailed.next(true);
+                return;
+              }
+              this.userData = data[0];
+              this.dataEdited.next(true);
+            }
+          },
+          (error) => {
+            console.log(error);
+            this.dataLoadFailed.next(true);
+            this.dataLoaded.next(null);
+          }
+        );
+    });
   }
   onDeleteData() {
     this.dataLoadFailed.next(false);
+
     this.http
-      .delete("https://API_ID.execute-api.REGION.amazonaws.com/dev/", {
-        headers: new Headers({ Authorization: "XXX" }),
-      })
+      .delete(
+        "https://96mex4cj0j.execute-api.ap-south-1.amazonaws.com/comp-dev/comparison",
+        {
+          headers: new Headers({
+            Authorization: "XXX",
+          }),
+        }
+      )
       .subscribe(
         (data) => {
           console.log(data);
